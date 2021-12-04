2d8123fab368790546b7b52c9370cc4070da44ad
martinetmayank <thedeveloper.mayank@gmail.com>
2021-12-04 16:39:47 +0530

    added file upload feature

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask.helpers import flash
 from werkzeug.utils import secure_filename
 
 from predict import predict
@@ -6,19 +8,48 @@ from predict import predict
 app = Flask(__name__)
 
 
+UPLOAD_FOLDER = '/'
+ALLOWED_EXT = set(['png', 'jpg', 'jpeg', 'bmp'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXT
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    print(filename)
+    return send_from_directory(
+        app.config['UPLOAD_FOLDER'],
+        filename
+    )
+
+
 @app.route('/predict', methods=['GET', 'POST'])
 def result():
     if request.method == 'POST':
-        filename = request.files['file']
-        filename = secure_filename(filename.filename)
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect('/')
 
-        filepath = 'static/images/' + filename
-        # print(filename)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect('/')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        filepath = url_for('uploaded_file', filename=filename)
+        filepath = f'http://localhost:{PORT}{filepath}'
+        print('\n', filepath, '\n')
 
         description = predict(filepath)
         description = description[6:-4]
@@ -27,9 +58,31 @@ def result():
             "filename": filename,
             "description": description
         }
+
         return render_template('result.html', result=output)
-    return None
+    # return redirect(/)
+
+
+# @app.route('/predict', methods=['GET', 'POST'])
+# def result():
+#     if request.method == 'POST':
+#         filename = request.files['file']
+#         filename = secure_filename(filename.filename)
+
+#         filepath = 'static/images/' + filename
+#         # print(filename)
+
+#         description = predict(filepath)
+#         description = description[6:-4]
+
+#         output = {
+#             "filename": filename,
+#             "description": description
+#         }
+#         return render_template('result.html', result=output)
+#     return None
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    PORT = 10000
+    app.run(debug=True, port=PORT)
